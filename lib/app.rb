13161c161084fc574d67dc6c7645fcea284fa5a1
AlecDWalker <walkeralec@hotmail.com>
2020-08-11 13:44:38 +0100

    fourth test passing, capitalisation accounted for

--- lib/app.rb
+++ lib/app.rb
@@ -1,8 +1,8 @@
 def spellchecker(string)
-  dictionary = ['Ball', 'Red']
+  dictionary = ['ball', 'red']
   array = string.split
   return array.map { |word|
-    if dictionary.include?(word)
+    if dictionary.include?(word.downcase)
       word = word
     else
     word = "~#{word}~"
