fcd7e84b741ae47dc6889bf46038d4561c4a91b5
AlecDWalker <walkeralec@hotmail.com>
2020-08-11 13:16:25 +0100

    second test failing

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,4 +5,8 @@ describe 'spellchecker' do
   it 'should be able to return a string that needs no correction' do
     expect(spellchecker("Hello world")).to eq "Hello world"
   end
+
+  it 'should be able to return a highlighted error with a single word' do
+    expect(spellchecker('Balf')).to eq '~Balf~'
+  end
 end
